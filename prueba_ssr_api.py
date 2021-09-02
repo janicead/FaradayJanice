be672b3fddf2a6e4a2dfc8a0bfb4e7bdb633a5b5
janice <janicefabulus@live.com.ar>
2021-09-01 23:11:15 -0300

    Cambios en endpoints

--- prueba_ssr_api.py
+++ prueba_ssr_api.py
@@ -1,9 +1,9 @@
 import json
 
-from flask import Flask, request, make_response, session, redirect, url_for
+from flask import Flask, request, make_response, redirect, url_for
 import requests
 
-
+session = []
 app = Flask(__name__)
 app.secret_key = 'thisismysecretkey'
 
@@ -14,36 +14,90 @@ def error(error_dict, status=400):
 
 @app.route("/members_only")
 def members_only():
-    if "user" not in session:
-        return redirect(url_for("login"))
+    if request.json['user'] not in session:
+        return redirect(url_for("login_get"))
     my_id = request.args.get('id')
     if my_id is None or len(my_id) == 0:
-        return error({'error': 'El id no fue ingresado'})
+        return error({'error': 'You have to send the id'})
     base_url = "https://jsonplaceholder.typicode.com/posts?id="
     mi_url = base_url + my_id
     r = requests.get(url=mi_url)
     data = json.loads(r.text)
-    print(mi_url)
     return data[0]
 
 
+def _check_correct_user_and_pass(user, password):
+    for element in users:
+        if element["user"] == user and element["password"] == password:
+            return True
+    return False
 
-@app.route("/login", methods=["POST","GET"])
-def login():
-    if request.method == "POST":
-        # _check_correct_user_and_pass(request.form)
-        session["user"] = request.form
-        return redirect(url_for("user"))
+
+def _user_already_exists(user):
+    for element in users:
+        if element['user'] == user:
+            return True
+    return False
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    if _check_correct_user_and_pass(request.json['user'], request.json['password']):
+        session.append(request.json['user'])
+        return {
+            "message": "Successfull login"
+        }
     else:
         return {
+            "message": "Wrong user or password"
+        }
+
+
+@app.route("/login")
+def login_get():
+    return {
             "message": "You have to login"
         }
 
 
-@app.route("/logout")
+@app.route("/logout", methods=["POST"])
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
+    if request.json['user'] not in session:
+        return redirect(url_for("login_get"))
+    session.remove(request.json['user'])
+    return {
+            "message": "Successfull logout"
+        }
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if _user_already_exists(request.json["user"]):
+        return error({'error': 'This user already exists'})
+    else:
+        users.append({
+            "user": request.json["user"],
+            "password": request.json["password"]
+        })
+        return {"message": "Now you can login"}
+
+
+def _user_in_session(my_user):
+    for user in session:
+        if user['user'] == my_user:
+            return True
+
+
+users = [
+    {
+        "user": "admin",
+        "password": "admin"
+    },
+    {
+        "user": "admin2",
+        "password": "admin2"
+    }
+]
 
 
 @app.route("/user")
